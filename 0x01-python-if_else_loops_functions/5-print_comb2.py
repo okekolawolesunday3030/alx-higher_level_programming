d1054138dbc521f403c5b63e6d5e96755cc951dc
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-11-22 16:37:41 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3                                     
+# progrom that prints 0 to 99                          
+for i in range(0, 99):       
+    print("{} = {}".format(number, hex(number)) 
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3                                                  
+# prrogarm that prints capital letter                               
+def uppercase(str):                                                 
+    for u in str:                                                   
+        if ord(u) >= 97 and ord(u) <= 122:                          
+            u = chr(ord(u) - 32) 
+                                            
+        print("{}".format(u), end = "")
+    print("")
