caffd289033129c026ad7d3222924517f844182e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-12-12 02:23:39 +0000

    all

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """prints all intergers of a list"""
     for j in range(my_list):
-        print('{:d}'.format(my_list[j]))
+        print("{:d}".format(my_list[j]))
