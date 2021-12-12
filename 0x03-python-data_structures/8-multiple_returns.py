125fc6df62c6ee57a52a0442ac20b3387a2cd566
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-12-12 02:22:04 +0000

    all

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """function that returns a tuple with the length of a string and its first character."""
+    if len(sentence) == 0:
+       return (None)
+
+    firstLet = sentence[0]
+      for i in range(len(sentence):
+          if sentence[i] < firstLet:
+            firstLet = my_list[i]
+
+      return (firstLet)
