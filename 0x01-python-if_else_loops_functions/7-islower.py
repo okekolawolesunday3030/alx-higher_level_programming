3c45bfdfacb2a01b2df805a1b9bfe0f53c6ee77a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-01-01 09:32:20 +0000

    if

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):                                                     
     """Check for lowercase characters."""                           
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True                                                    else:                                        
-        return False  
+    if ord(c) >= 97:
+        return True                                                    elif ord(c) <= 122:
+          return False  
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):                                                 
     for u in str:                                                   
         if ord(u) >= 97 and ord(u) <= 122:                          
-            u = chr(ord(u) - 32) 
+             u = chr(ord(u) - 32) 
                                             
-        print("{}".format(u), end = "")
+        print("{}".format(u), end="")
     print("")
