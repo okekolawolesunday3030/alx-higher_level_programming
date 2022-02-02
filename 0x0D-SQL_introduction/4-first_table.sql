2792beda61fd00536dfda25e0506f2a29d9a3156
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-02-02 12:51:31 +0000

    push

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- script that lists all the tables of a database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL s rst_table;
+INSERT INTO first_table(id, name) VALUES(89, 'Best School')
