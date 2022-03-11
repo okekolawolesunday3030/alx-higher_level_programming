81f72105c7c79b6f18e2787b3bc179411893216b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-03-11 14:55:19 +0000

    all

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,9 @@
 -- writing a script that that wile show all the records where name   califonia
 
 
+
+SELECT * FROM cities
+WHERE state_id = (
+SELECT id FROM states WHERE name ='California')
+ORDER BY id;
+
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+
+
+SELECT cities.id, cities.name  states.name FROM cities 
+INNER JOIN states ON cities.id = states.id
+ORDER BY cities.id;  
