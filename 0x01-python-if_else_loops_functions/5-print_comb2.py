c97b2d653cc5d7acb786f3cf25f9d52d2624c514
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-03-13 07:38:59 +0000

    a

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3                                     
 # progrom that prints 0 to 99                          
-for i in range(0, 99):       
-    print("{} = {}".format(number, hex(number)) 
+for i in range(00, 99):       
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i +1))
