357eb1a8e6c35e0f5fead6030e3a119e39bd2b26
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-03-14 01:41:48 +0000

    addition

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def islower(c):                                                     
-    """Check for lowercase characters."""                           
-    if ord(c) >= 9:
-          return True
-      elif ord(c) <= 122:
-            return False  
+def islower(c):
+	"""Check for lowercase characters."""
+         if ord(c) >= 9:
+	     return True
+         elif ord(c) <= 122:
+              return False  
