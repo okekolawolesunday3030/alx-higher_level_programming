de6cf9ae7c72b59acde0ed472558ac1bec91eb99
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-06-20 12:26:58 -0700

    webscrp

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const conten = process.argv[3];
+
+fs.writeFile(file, content, 'utf-8', function (err, data) {
+	if (err) {
+		console.log(err);
+	}
+});
