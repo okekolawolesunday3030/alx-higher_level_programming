e891e758dbdbebf73b44a94a4fa6f0ab86c66b67
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-06-20 13:45:24 -0700

    py print_comb pep

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """program that prints aphabet escept q and e
 """
-for i in range(97, 123) :
-     if i != 113 and i != 101:
-         print("{}".format(chr(i)), end="")
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3                                     
-# progrom that prints 0 to 99                          
-for i in range(00, 99):       
+#!/usr/bin/python3
+# progrom that prints 0 to 99
+for i in range(00, 99):
     print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i +1))
+print("{:02d}".format(i + 1))
