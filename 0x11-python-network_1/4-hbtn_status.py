7634866b838f2da3c36653e67588d0259dfc71d7
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-06-20 01:05:00 -0700

    all

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""script that fetches https://intranet.hbtn.io/status"""
+
+import requests
+
+
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type :', type(r.texxt))
+    print('\t-  content :',r.text)
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""script that takes in a URL and
+an email address, send post"""
+
+from sys import argv
+import requests
+
+if __ts.post(argv[1], data={'email': argv[2]}
+        r =requests.post(argv[1], data={'email': argv[2]})
+        print(r.text)
