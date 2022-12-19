8b43ffced5ee831870cff2d11216620a81041f8b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-19 20:07:12 +0000

    alxjsismad

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 
-const myArgs = process.argv.slice(2)
-
-console.log(myArgv[0] + 'Is' + myArgs[1]);
+const myArgs = process.argv.slice(2);
+console.log(myArgs[0] + 'is' + myArgs[1]);
