5e5e8e5100187d056b14efaacd7794af5bc11f5b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-19 19:50:54 +0000

    standardcode

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,10 @@
 const myArgs = process.argv.slice(2);
 const length = myArgs.length;
 
-if (length <= 0){
-	console.log('No argument');
+if (length <= 0) {
+  console.log('No argument');
+} if (length === 1) {
+  console.log('Argument found');
+} if (length > 1) {
+  console.log('Arguments found');
 }
-if (length === 1){
-	console.log('Argument found')
-}
-if (length > 1){
- 	console.log('Arguments found')
-}
-
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const  myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2);
 
- if (myArgs[0] === undefined){
-      console.log('No argument');
-} else{
-      console.log(myArgs[0]);
+if (myArgs[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(myArgs[0]);
 }
