1473af49ae4058fb666b41c5ae57bc9146def080
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-19 20:14:54 +0000

    standardjs

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,8 @@
 
 const myArgs = process.argv.slice(2);
 
-if (isNaN(myArgs[0])){
-	console.log('Not a number');
-}else{
-
-        console.log('my number: ' + parseInt(myArgs[0]));
+if (isNaN(myArgs[0])) {
+  console.log('Not a number');
+} else {
+  console.log('my number: ' + parseInt(myArgs[0]));
 }
