08a314e273b2f2fdbb6dba41bd8d2abe768608f3
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-21 17:58:50 +0000

    output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,5 +7,5 @@ request(url, function(error, response) {
   if (error)
     console.error('error:', error);
   else
-    console.log('code:',response && response.statusCode);
+    console.log('code:', response && response.statusCode);
 });
