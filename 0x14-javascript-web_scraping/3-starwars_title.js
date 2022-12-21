edf95b47183b948eaf50b2f7d0dc61b05bf64416
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-21 17:54:50 +0000

    title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
 
-const reuest =  require('request');
+const request =  require('request');
+const number = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + number, function (error, response, body) {
+  if (error)
+    console.error('error:', error);
+  else if (response.statusCode === 200) {
+     const jsonobj = JSON.parse(body);
+     console.log(jsonobj.title);
+  }
+});
