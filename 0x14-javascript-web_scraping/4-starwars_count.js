0a70aca143d5fffd57cd4873d7ab3204b1e19d18
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-21 18:22:43 +0000

    json

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,24 @@
-:wq
+#!/usr/bin/node
 
+const request =  requirie('request');
+const path = process.argv[2];
+
+
+request(url, function (error, response, body) {
+  if (error)
+    console.error('error:', error);
+  else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+ 
+    for (let i = 0; i < films.length; i++) {
+      const filmName = films[i].characters;
+      for (let j = 0; j < i; j++) {
+        if (fileName[j].includes('18')) {
+         count++;
+        }
+      }
+    }
+    console.log(count);
+   }
+});
